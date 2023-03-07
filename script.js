8e59fe247bd58ba4a73b066ad2859ae4b5f4b951
Isaac Grossberg <igrossberg@conceptsystemsinc.com>
2023-03-06 17:00:41 -0800

    update about me section

--- script.js
+++ script.js
@@ -3,6 +3,62 @@
 const copyrightEl = document.querySelector(".copyright");
 const linkResume = document.querySelector(".resume--link");
 
+// const aboutRightContainer = document.querySelector(".about--right-container");
+// const aboutLeftContainer = document.querySelector(".about--left-container");
+const aboutGrid = document.getElementById("about-grid");
+const aboutCenterContainer = document.querySelector(".about--center-container");
+
+const ABOUT_ME = [
+  {
+    icon: "settings-outline",
+    title: "Controls Engineering Intern",
+    description: "Worked under a senior process engineer for a small engineering firm, aiding in schematic design and PLC programming",
+    date: "Jun '16",
+  },
+  {
+    icon: "hardware-chip-outline",
+    title: "Intern @ Intel",
+    description: "Worked on a variety of projects ranging from hardware failure analysis to web application development",
+    date: "Mar '19",
+  },
+  {
+    icon: "school-outline",
+    title: "OSU Grad",
+    description: "3.95 GPA in Electrical & Computer Engineering (minor in Computer Science)",
+    date: "Jun '20",
+  },
+  {
+    icon: "flash-outline",
+    title: "Intern @ SSOE",
+    description: "Learned power distribution engineering in construction projects for large tech companies",
+    date: "Jun '20",
+  },
+  {
+    icon: "battery-charging-outline",
+    title: "Electrical Engineer @ SSOE",
+    description: "Worked on a variety of projects doing 3D modeling, lighting calcs, and electrical design",
+    date: "Dec '20",
+  },
+  {
+    icon: "desktop-outline",
+    title: "Career Change - Web Dev",
+    description: "Independently learned HTML, CSS, JavaScript, and React",
+    date: "Jan '22",
+  },
+  {
+    icon: "code-slash-outline",
+    title: "Web Developer @ CSI",
+    description: "Worked on two web applications using technologies like React, Laravel, MySQL, and Docker",
+    date: "May '22",
+  },
+  {
+    icon: "paper-plane-outline",
+    title: "Onward & Upward",
+    description: "Looking for that next opportunity for growth and development",
+    date: "Present",
+  }
+]
+
 const WINDOW_MIN_WIDTH = 600;
 
 function generateCopyright() {
@@ -14,9 +70,69 @@ function generateCopyright() {
 }
 generateCopyright();
 
-linkResume.addEventListener("click", function (e) {
-  if (window.innerWidth < WINDOW_MIN_WIDTH) {
-    e.preventDefault();
-    window.location.href = "./docs/Grossberg-resume-03-22.pdf";
-  }
+// Window load
+window.addEventListener("load", function () {
+  linkResume.addEventListener("click", function (e) {
+    if (window.innerWidth < WINDOW_MIN_WIDTH) {
+      e.preventDefault();
+      window.location.href = "./docs/Grossberg-resume-03-22.pdf";
+    }
+  });
+
+  buildAboutMe();
 });
+
+function buildAboutMe() {
+  // Build out each section
+  ABOUT_ME.forEach((section, idx) => {
+
+    /** Build side elements **/
+    const aboutBox = document.createElement("div");
+    aboutBox.className = "about--box";
+
+    const aboutIcon = document.createElement("ion-icon");
+    aboutIcon.className = "about--icon";
+    aboutIcon.name = section.icon;
+
+    const aboutText = document.createElement("div");
+    aboutText.className = "about--text";
+
+    const aboutTextMain = document.createElement("h3");
+    aboutTextMain.className = "about--text-main";
+    aboutTextMain.innerHTML = section.title;
+
+    const aboutTextSub = document.createElement("p");
+    aboutTextSub.className = "about--text-sub";
+    aboutTextSub.innerHTML = section.description;
+
+    const mobileDate = document.createElement("p");
+    mobileDate.innerText = section.date;
+    mobileDate.className = "mobile-date";
+
+    /** Combine side elements **/
+    aboutBox.appendChild(aboutIcon);
+    aboutBox.appendChild(aboutText);
+    aboutText.appendChild(mobileDate);
+    aboutText.appendChild(aboutTextMain);
+    aboutText.appendChild(aboutTextSub);
+
+    /** Build timeline elements **/
+    const aboutTimelineText = document.createElement("div");
+    aboutTimelineText.className = "about--timeline-text";
+
+    const aboutTimelineTextP = document.createElement("p");
+    aboutTimelineTextP.innerText = section.date;
+
+    const aboutTimelineTextDiv = document.createElement("div");
+
+    /** Combine timeline elements **/
+    aboutTimelineText.appendChild(aboutTimelineTextP);
+    aboutTimelineText.appendChild(aboutTimelineTextDiv);
+
+    /** Add everything to the DOM **/
+    aboutGrid.appendChild(aboutBox);
+    aboutCenterContainer.appendChild(aboutTimelineText);
+
+  });
+}
+
